5704d2dc5e0c7ed698973d316d1dfd80e465c900
hcschool <dongul11@hollandchristian.org>
2017-12-18 07:59:21 -0500

    ex3-5

--- ex2.py
+++
@@ -1,6 +0,0 @@
-# This is a comment! IT comment on codes.
-# It's really important
-print ('This is a comment file.')
-
-# Comment on what we do
-print ('We practice commenting files!')
---
+++ test.py
@@ -0,0 +1,1 @@
+print("Let's talk about {my_name}.")
